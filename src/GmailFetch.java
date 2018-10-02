d22c42d7c8d5fca5dec3b5a2dad69cb7310494e9
johnchandlerphillips <johnchandlerphillips@172.26.80.230>
2018-10-02 12:33:31 -0500

    Added a JOptionPane to request username and password from the user

--- src/GmailFetch.java
+++ src/GmailFetch.java
@@ -4,40 +4,47 @@
 import javax.mail.*;
 import javax.mail.search.FlagTerm;
 import java.util.*;
+import javax.swing.JOptionPane;
 
 public class GmailFetch {
 
     public static void main(String[] args) throws Exception {
 
-        String content[];
+        String username = "";
+        String password = "";
+        //I have a lot to work on this, this will be replaced soon.
+        username = JOptionPane.showInputDialog("Email: ");
+        password = JOptionPane.showInputDialog("Password: ");
 
-        Session session = Session.getDefaultInstance(new Properties());
-        Store store = session.getStore("imaps");
-        store.connect("imap.googlemail.com", 993, "EmailHERE@murraystate.edu", "passwordHERE");
-        Folder inbox = store.getFolder("INBOX");
-        inbox.open(Folder.READ_ONLY);
+        if (username != "" && password != "") {
+            Session session = Session.getDefaultInstance(new Properties());
+            Store store = session.getStore("imaps");
+            store.connect("imap.googlemail.com", 993, username, password);
+            Folder inbox = store.getFolder("INBOX");
+            inbox.open(Folder.READ_ONLY);
 
-        // Fetch unseen messages from inbox folder
-        Message[] messages = inbox.search(
-                new FlagTerm(new Flags(Flags.Flag.SEEN), false));
+            // Fetch unseen messages from inbox folder
+            Message[] messages = inbox.search(
+                    new FlagTerm(new Flags(Flags.Flag.SEEN), false));
 
-        // Sort messages from recent to oldest
-        Arrays.sort(messages, (m1, m2) -> {
-            try {
-                return m2.getSentDate().compareTo(m1.getSentDate());
-            } catch (MessagingException e) {
-                throw new RuntimeException(e);
+            // Sort messages from recent to oldest
+            Arrays.sort(messages, (m1, m2) -> {
+                try {
+                    return m2.getSentDate().compareTo(m1.getSentDate());
+                } catch (MessagingException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+
+            for (Message message : messages) {
+                System.out.println(
+                        "Date Received: " + message.getSentDate()
+                        + "\nEmail Subject: " + message.getSubject()
+                        + "\n" + "Content of email: \n" + message.getContent() + "\n");
+                String s = message.getContent().toString();
+                String[] ss = s.split(" ");
+                System.out.println(Arrays.toString(ss));
             }
-        });
-
-        for (Message message : messages) {
-            System.out.println(
-                    "Date Received: " + message.getSentDate()
-                    + "\nEmail Subject: " + message.getSubject()
-                    + "\n" + "Content of email: \n" + message.getContent() + "\n");
-            String s = message.getContent().toString();
-            String[] ss = s.split(" ");
-            System.out.println(Arrays.toString(ss));
         }
     }
 }
